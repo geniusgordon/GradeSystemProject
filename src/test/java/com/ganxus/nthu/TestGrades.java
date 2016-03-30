9c38fc770ad2094323b92226238f66a60ae6053f
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-30 22:15:02 +0800

    finish header

--- src/test/java/com/ganxus/nthu/TestGrades.java
+++ src/test/java/com/ganxus/nthu/TestGrades.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 import com.ganxus.nthu.Grades;
 
 public class TestGrades {
+  /**
+   * Test if the grades calculate with weights return correct value.
+   *
+   */
 
   @Test
   public void testCalculateTotalGrade() {
@@ -11,6 +15,20 @@ public class TestGrades {
     float weights[] = { 0.1f, 0.1f, 0.1f, 0.3f, 0.4f };
     int totalGrade = grade.calculateTotalGrade(weights);
     assertEquals(100, totalGrade);
+  }
+
+  /**
+   * Test the override function "equals" have the correct compare between Grade Object.
+   *
+   */   
+  @Test
+  public void testEquals(){
+    int grades[] = {100, 100, 100, 100, 100};
+    Grades grade1 = new Grades("Gordon","102062312",grades);
+    Grades grade2 = new Grades("Gordon","102062312",grades);
+    
+    boolean checkEqual = grade1.equals(grade2);
+    assertEquals(true, checkEqual);
   }
 }
 
